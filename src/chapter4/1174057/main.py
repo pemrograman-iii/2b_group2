f02f8d9bea94db580be3b1ac3244f8eb238f2af6
alitkurniawan48 <alitfajark@gmail.com>
2019-03-27 03:11:32 +0700

    Do'a Ibu

---
+++ src/chapter4/1174057/main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 21:40:17 2019
+
+@author: alitfajarkurniawan
+"""
+
+lib = __import__('1174057csv')
+
+lib.bukaModeDictCsv()
+
+lib.tulisCsv()
---
+++ src/chapter4/1174057/main2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 21:40:25 2019
+
+@author: alitfajarkurniawan
+"""
+
+lib = __import__('1174057pandas')
+
+lib.bukaModeListPandas()
+lib.bukaModeDictPandas()
+
+lib.tulisCsvPandas()
