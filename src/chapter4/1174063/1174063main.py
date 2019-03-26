786373662adf89e48d8248373ba7d2068803e5f7
iqbalpanggabean <gabeiqbal@gmail.com>
2019-03-27 03:52:59 +0700

    jadi otakku yang error bang

---
+++ src/chapter4/1174063/1174063main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 27 02:52:39 2019
+
+@author: Muhammad iqbal
+"""
+
+lib = __import__('1174063csv')
+
+lib.bukaModeDictCsv()
+
+lib.tulisCsv()
---
+++ src/chapter4/1174063/1174063main2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 27 02:53:47 2019
+
+@author: Muhammad iqbal
+"""
+
+lib = __import__('1174063pandas')
+
+lib.bukaModeListPandas()
+lib.bukaModeDictPandas()
+
+lib.tulisCsvPandas()
