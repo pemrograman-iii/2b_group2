7c3995cd9d933800acd180a13a9e531e10cbf874
alitkurniawan48 <alitfajark@gmail.com>
2019-04-02 21:56:05 +0700

    sayang Ibu

---
+++ src/chapter5/teori/1174057.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  2 20:59:08 2019
+
+@author: alitfajarkurniawan
+"""
+import serial
+
+def cobaarduino():
+	ser = serial.Serial("COM4",9600)
+	print(ser.name)
+
+cobaarduino()
